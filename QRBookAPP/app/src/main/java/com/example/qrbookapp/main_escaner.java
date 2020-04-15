49f0a173cbe5548999cd641cb164ed0f2bccc81f
RodriESM <45097575+RodriESM@users.noreply.github.com>
2020-04-15 20:29:05 +0200

    Android
    Escaner solucionado, falta añadir el meta(barcode) en el manifest y solicitar al usuario los permisos de la cámara

--- QRBookAPP/app/src/main/java/com/example/qrbookapp/main_escaner.java
+++ QRBookAPP/app/src/main/java/com/example/qrbookapp/main_escaner.java
@@ -1,22 +1,34 @@
 package com.example.qrbookapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.app.Activity;
 import android.widget.Toast;
-
 import com.blikoon.qrcodescanner.QrCodeActivity;
+
+
 
 public class main_escaner extends AppCompatActivity {
 
-    private static final int REQUEST_CODE_QR_SCAN = 300;
+    private static final int REQUEST_CODE_QR_SCAN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_escaner);
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT > 22) {
+                if (shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA))
+                    Toast.makeText(getApplicationContext(), "Esta aplicación necesita acceder a la cámara para funcionar", Toast.LENGTH_SHORT).show();
+                requestPermissions(new String[]{android.Manifest.permission.CAMERA}, REQUEST_CODE_QR_SCAN);
+            }
+        }
     }
 
     public void onClick(View v) {
