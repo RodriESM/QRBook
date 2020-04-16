98394a9bb615d1b6d4ea71b2779821444f775b05
Ivistark <58076477+Ivistark@users.noreply.github.com>
2020-04-16 20:16:37 +0200

    SplashScreen

---
+++ QRBookAPP/app/src/main/java/com/example/qrbookapp/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.qrbookapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent nuevaActividad = new Intent(getApplicationContext(), MainActivity.class);
+                //Intent intentPri = new Intent().setClass(SPlashScreeeen.this, MainActivity.class);
+                startActivity(nuevaActividad);
+                finish();
+            }
+        };
+
+        Timer timer= new Timer();
+        timer.schedule(task,2000);
+    }
+
+}
