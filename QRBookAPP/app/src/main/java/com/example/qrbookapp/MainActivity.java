432d2406d3a41cc072950a6b48dc1107b9970611
RodriESM <45097575+RodriESM@users.noreply.github.com>
2020-05-12 19:36:56 +0200

    Cierre indeseado al ir hacia el inicio solucionado

--- QRBookAPP/app/src/main/java/com/example/qrbookapp/MainActivity.java
+++ QRBookAPP/app/src/main/java/com/example/qrbookapp/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i= new Intent(MainActivity.this,RegistroActivity.class);
                 startActivity(i);
-                finish();
             }
         });
     }
