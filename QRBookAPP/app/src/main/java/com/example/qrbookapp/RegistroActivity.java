dda6ae1d6ba83ce248cfd5ba56bdec7c6ebb2598
Ivistark <58076477+Ivistark@users.noreply.github.com>
2020-05-13 20:06:29 +0200

    Insertar usuario en la base de datos desde android

--- QRBookAPP/app/src/main/java/com/example/qrbookapp/RegistroActivity.java
+++ QRBookAPP/app/src/main/java/com/example/qrbookapp/RegistroActivity.java
@@ -9,87 +9,65 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.qrbookapp.Database.ConnectionClass;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class RegistroActivity extends AppCompatActivity {
 
-    Connection conexionMySQL;
+    ConnectionClass conexionMySQL = new ConnectionClass();
     Button btnRegistrarse;
-    private String usuario, password, ip, bdDatos;
-    EditText etEmail,etPassword,etPassword2;
+    EditText etEmail,etUsuario,etPassword,etPassword2;
     private int puerto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
+        etEmail=findViewById(R.id.etEmail);
+        etUsuario=findViewById(R.id.etUsuario);
+        etPassword=findViewById(R.id.etPassword);
+        etPassword2=findViewById(R.id.etPassword2);
+
+
         btnRegistrarse=findViewById(R.id.btnRegistrarse);
 
         btnRegistrarse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i= new Intent(RegistroActivity.this,MainActivity.class);
-                startActivity(i);
-                finish();
+
+                String email= etEmail.getText().toString();
+                String ps1= etPassword.getText().toString();
+                String ps2= etPassword2.getText().toString();
+                String usuario=etUsuario.getText().toString();
+
+                if (ps1.equals(ps2)) {
+                    try {
+
+                        Connection f = ConnectionClass.con;
+                        PreparedStatement ps = f.prepareStatement("insert into USUARIO(correo,password,usuario) values(?,?,?)");
+                        ps.setString(1,email);
+                        ps.setString(2,ps1);
+                        ps.setString(3,usuario);
+                        ps.executeUpdate();
+                        Intent i = new Intent(RegistroActivity.this, MainActivity.class);
+                        startActivity(i);
+
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }else{
+
+                    Toast.makeText(getApplicationContext(),"Las contraseñas introducidas no son iguales.Intentelo de nuevo",Toast.LENGTH_LONG);
+
+                }
+
             }
         });
 
-
-
-       /* btnSignin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-
-                try {
-                    if(etPassword.getText().equals(etPassword2.getText())) {
-                        conectarBDMySQL(usuario, password, ip, puerto, bdDatos);
-                        Statement st = conexionMySQL.createStatement();
-                        st.executeQuery("insert into usuarios (correo,password) values '" + etEmail.getText() + "','" + etPassword.getText() + "'");
-                        Toast.makeText(getApplicationContext(),"Enhorabuena, se ha registrado en nuestra aplicación.Sea bienvenido",Toast.LENGTH_LONG);
-                    }
-                    else{
-                        Toast.makeText(getApplicationContext(),"Las contraseñas introducidas no son iguales.Intentelo de nuevo",Toast.LENGTH_LONG);
-                    }
-
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
-
-        });
-
-    }
-
-    public void conectarBDMySQL (String usuario, String contrasena,
-                                 String ip, int puerto, String bdDatos)
-    {
-        if (conexionMySQL == null) {
-            String urlConexionMySQL = "";
-            if (bdDatos != "")
-                urlConexionMySQL = "jdbc:mysql://" + ip + ":" + puerto + "/" + bdDatos;
-            else
-                urlConexionMySQL = "jdbc:mysql://" + ip + ":" + puerto;
-            if (usuario != "" & contrasena != "" & ip != "" & puerto != 0) {
-                try {
-                    Class.forName("com.mysql.jdbc.Driver");
-                    conexionMySQL = DriverManager.getConnection(urlConexionMySQL,
-                            usuario, contrasena);
-                } catch (ClassNotFoundException e) {
-                    Toast.makeText(getApplicationContext(),
-                            e.getMessage(),
-                            Toast.LENGTH_LONG).show();
-                } catch (SQLException e) {
-                    Toast.makeText(getApplicationContext(),
-                            e.getMessage(),
-                            Toast.LENGTH_LONG).show();
-                }
-            }
-        }*/
     }
 }
