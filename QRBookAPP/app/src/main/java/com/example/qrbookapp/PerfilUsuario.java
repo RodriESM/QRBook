e2d5da5b5d099186f8790b359b719c3f75b62f66
Ivistark <58076477+Ivistark@users.noreply.github.com>
2020-05-19 18:38:59 +0200

    Modificación de los datos de usuario

--- QRBookAPP/app/src/main/java/com/example/qrbookapp/PerfilUsuario.java
+++ QRBookAPP/app/src/main/java/com/example/qrbookapp/PerfilUsuario.java
@@ -8,9 +8,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.example.qrbookapp.Database.ConnectionClass;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class PerfilUsuario extends AppCompatActivity {
 
-    EditText etUsuario,etNombre,etApellido1,etApellido2,etContrasena,etRepiteContrasena;
+    EditText etUsuarioCam,etNombre,etApellido1,etApellido2,etContrasena,etRepiteContrasena;
     ImageView imgUsuario;
     Button btnRealizarCambios;
 
@@ -19,15 +25,43 @@ public class PerfilUsuario extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_usuario);
 
-        etUsuario=findViewById(R.id.etUsuario);
-        etNombre=findViewById(R.id.etNombre);
-        etApellido1=findViewById(R.id.etApellido1);
-        etApellido2=findViewById(R.id.etApellido2);
-        etContrasena=findViewById(R.id.etContrasena);
-        etRepiteContrasena=findViewById(R.id.etRepiteContrasena);
-        imgUsuario=findViewById(R.id.imgUsuario);
-        btnRealizarCambios=findViewById(R.id.btnRealizarCambios);
+        etUsuarioCam = findViewById(R.id.etUsuarioCam);
+        etNombre = findViewById(R.id.etNombre);
+        etApellido1 = findViewById(R.id.etApellido1);
+        etApellido2 = findViewById(R.id.etApellido2);
+        etContrasena = findViewById(R.id.etContrasena);
+        etRepiteContrasena = findViewById(R.id.etRepiteContrasena);
+        imgUsuario = findViewById(R.id.imgUsuario);
+        btnRealizarCambios = findViewById(R.id.btnRealizarCambios);
 
+        String usuario = etUsuarioCam.getText().toString();
+        String nombre = etNombre.getText().toString();
+        String apellido1 = etApellido1.getText().toString();
+        String apellido2 = etApellido2.getText().toString();
+        String contrasena = etContrasena.getText().toString();
+        String repContrasena = etRepiteContrasena.getText().toString();
+
+        try {
+            Connection connection = ConnectionClass.con;
+            PreparedStatement ps = null;
+            if (contrasena.equals(repContrasena)) {
+                ps = connection.prepareStatement("update USUARIO set usuario=?,password=?,nombre=?,apellido1=?,apellido2=? where correo=?");
+
+
+                ps.setString(1, usuario);
+                ps.setString(2, contrasena);
+                ps.setString(3, nombre);
+                ps.setString(4, apellido1);
+                ps.setString(5, apellido2);
+                ps.setString(6, "1234");
+
+                ps.executeUpdate();
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -36,5 +70,6 @@ public class PerfilUsuario extends AppCompatActivity {
             super.finish();
         }
         return super.onKeyDown(keyCode, event);
+
     }
 }
